eedc42fd98f51cec1072e8db9043abd1e449757d
hinst <hinstance@yandex.ru>
2024-09-28 15:58:16 +0200

    Use PngCrush -ow option to avoid requesting a permission from Deno runtime. Differentiate between total size and compressed size. Cache: store both sizes, before and after

--- main.ts
+++ main.ts
@@ -1,14 +1,22 @@
 import "jsr:@std/dotenv/load"
 import { prettyBytes } from "https://deno.land/x/pretty_bytes@v2.0.0/mod.ts"
 
+class FileInfo {
+	constructor(public sizeBefore: number, public sizeAfter: number) {
+
+	}
+}
+
 class App {
 	static readonly PNG_CRUSH_PATH_ENV = 'PNG_CRUSH_PATH';
 	static readonly CACHE_FILE_NAME = 'cache.json';
 	private pngCrushPath: string;
 	/** File path -> compressed size */
-	private cache: Record<string, number> = {};
+	private cache: Record<string, FileInfo> = {};
 	private totalSizeBefore = 0;
 	private totalSizeAfter = 0;
+	private compressedSizeBefore = 0;
+	private compressedSizeAfter = 0;
 
 	constructor(private folder: string) {
 		if (!folder?.length)
@@ -31,15 +39,25 @@ class App {
 	}
 
 	async run() {
+		console.time('Total time');
 		this.loadCache();
 		await this.compressFolder();
+		console.timeEnd('Total time');
+		console.log('Total size');
+		console.log('  Before:', prettyBytes(this.totalSizeBefore));
+		console.log('  After:', prettyBytes(this.totalSizeAfter));
+		console.log('Compressed size');
+		console.log('  Before:', prettyBytes(this.compressedSizeBefore));
+		console.log('  After:', prettyBytes(this.compressedSizeAfter));
 	}
+
 
 	private loadCache() {
 		try {
 			if (Deno.statSync(this.cacheFilePath))
 				this.cache = JSON.parse(Deno.readTextFileSync(this.cacheFilePath));
-		} catch (e) {
+		} catch (_) {
+			// file not found
 		}
 	}
 
@@ -57,38 +75,38 @@ class App {
 			if (fileName.isFile && fileName.name.toLowerCase().endsWith('.png')) {
 				const filePath = this.folder + '/' + fileName.name;
 				const fileSize = Deno.statSync(filePath).size;
-				if (this.cache[filePath] === fileSize)
+				this.totalSizeBefore += fileSize;
+				if (this.cache[filePath]?.sizeAfter === fileSize)
 					++skippedCount;
 				else {
 					console.log('Compressing file:', filePath, prettyBytes(fileSize));
 					this.compressFile(filePath);
 				}
+				const fileSizeAfter = Deno.statSync(filePath).size;
+				this.totalSizeAfter += fileSizeAfter;
 			}
+			if (fileName.isDirectory)
+				await this.compressFolder(folder + '/' + fileName.name);
 		}
 		if (skippedCount)
 			console.log('  skipped', skippedCount, 'files');
 	}
 
-	private compressFile(sourceFilePath: string) {
-		const fileSizeBefore = Deno.statSync(sourceFilePath).size;
-		const postfix = (Math.random() * 999_999).toFixed();
-		const outputFilePath = sourceFilePath + '.' + postfix;
+	private compressFile(filePath: string) {
+		const fileSizeBefore = Deno.statSync(filePath).size;
 		const output = new Deno.Command(this.pngCrushPath,
-			{ args: [sourceFilePath, outputFilePath] }
+			{ args: ['-ow', filePath] }
 		).outputSync();
 		if (output.code === 0) {
-			const fileSizeAfter = Deno.statSync(outputFilePath).size;
-			Deno.removeSync(sourceFilePath);
-			Deno.renameSync(outputFilePath, sourceFilePath);
-
-			this.totalSizeBefore += fileSizeBefore;
-			this.totalSizeAfter += fileSizeAfter;
+			const fileSizeAfter = Deno.statSync(filePath).size;
+			this.compressedSizeBefore += fileSizeBefore;
+			this.compressedSizeBefore += fileSizeAfter;
 			const ratio = fileSizeAfter / fileSizeBefore;
-			this.cache[sourceFilePath] = fileSizeAfter;
+			this.cache[filePath] = new FileInfo(fileSizeBefore, fileSizeAfter);
 			this.saveCache();
 			console.log('\tdone', (ratio * 100).toFixed(1) + '%');
 		} else
-			console.error('Failed:', sourceFilePath, '=>', output.code,
+			console.error('Failed:', filePath, '=>', output.code,
 				'\n', new TextDecoder().decode(output.stdout),
 				'\n', new TextDecoder().decode(output.stderr));
 	}
